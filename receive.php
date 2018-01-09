5c3dd2395c4e9168bfc521896ecc06be69379566
junyuo <albert_kuo@cht.com.tw>
2018-01-09 14:18:36 +0800

    reply

--- receive.php
+++ receive.php
@@ -2,8 +2,9 @@
  $json_str = file_get_contents('php://input'); //接收REQUEST的BODY
  $json_obj = json_decode($json_str); //轉JSON格式
  //產生回傳給line server的格式
-	$sender_userid = $json_obj->events[0]->source->userId;
-	$sender_txt = $json_obj->events[0]->message->text;
+ $sender_userid = $json_obj->events[0]->source->userId;
+ $sender_txt = $json_obj->events[0]->message->text;
+ $sender_replyToken = $json_obj->events[0]->replyToken;
  $response = array (
 				"replyToken" => $sender_replyToken,
 				"messages" => array (
@@ -14,16 +15,16 @@
 				)
 		);
  $myfile = fopen("log.txt","w+") or die("Unable to open file!"); //設定一個log.txt 用來印訊息
- fwrite($myfile, "\xEF\xBB\xBF".json_decode($json_obj)); //在字串前加入\xEF\xBB\xBF轉成utf8格式
+ fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前加入\xEF\xBB\xBF轉成utf8格式
  fclose($myfile);
  //回傳給line server
-	$header[] = "Content-Type: application/json";
+ $header[] = "Content-Type: application/json";
 	$header[] = "Authorization: Bearer yHklpsNRGvLQA5tWPSpR7MpLlWNNS6OvoswjzBx3UlEiVTIsWlEKqQvpjcXfeCHmIYfdJk6/6BKqUCgyXMsFEmoaj6ZDIbbzw3HFCO5TQzmY7TX34FvU85+pTFZGbDm6bBJqsPyJsIkt/kiFVAbjpAdB04t89/1O/w1cDnyilFU=";
-	$ch = curl_init("https://api.line.me/v2/bot/message/reply");                                                                      
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);                                                                                                   
-	$result = curl_exec($ch);
-	curl_close($ch); 
+ $ch = curl_init("https://api.line.me/v2/bot/message/reply");                                                                      
+ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+ curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+ curl_setopt($ch, CURLOPT_HTTPHEADER, $header);                                                                                                   
+ $result = curl_exec($ch);
+ curl_close($ch); 
 ?>
