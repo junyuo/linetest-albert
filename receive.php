336c1903e1b4af29eb3d7b9df6e6cdd448fd2251
junyuo <albert_kuo@cht.com.tw>
2018-01-09 11:36:26 +0800

    commit

--- receive.php
+++ receive.php
@@ -1,3 +1,9 @@
 <?php
-echo "hello i'm albert"
+  echo "hello i'm albert"
+  $json_str = file_get_contents('php://input'); // 接收 request body
+  $json_obj = json_decode($json_str); // convert to JSON format
+
+  $myfile = fopen("log.txt", "w+") or die("unable to open file!");
+  fwrite($myfile,  "\xEF\xBB\xBf".$json_str); // 在字串前加入 \xEF\xBB\Bf 轉成 utf8 format
+  fclose($myfile);
 ?>
